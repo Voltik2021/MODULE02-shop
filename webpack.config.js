25b69c377e7752e2d72e6ea03683d52a5fac1169
voltik <voltik2019@yandex.ru>
2023-03-19 17:04:29 +0300

    sass

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlagin = require("mini-css-extract-plugin");
+
+const devServer = (isDev) =>
+  !isDev
+    ? {}
+    : {
+        devServer: {
+          open: true,
+          hot: true,
+          port: 8081,
+        },
+      };
+
+module.exports = ({ dev }) => ({
+  mode: dev ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlagin({
+      filename: "./styles/main.css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlagin.loader, "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlagin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  ...devServer(dev),
+});
